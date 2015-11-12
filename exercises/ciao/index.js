2508d3e89e1cf0743cde08b6568bef05fb3b576b
Mike Williams <mdub@dogbiscuit.org>
2015-11-12 04:27:56 +0100

    Environment example.

--- exercises/ciao/index.js
+++ exercises/ciao/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 // Constants
 var PORT = (process.env.PORT || "5000");
-var MESSAGE = (process.env.MESSAGE || "Hey, guys!");
+var MESSAGE = (process.env.MESSAGE || "Ciao mondo.");
 
 // SIGINT handler
 process.on('SIGINT', function() {
